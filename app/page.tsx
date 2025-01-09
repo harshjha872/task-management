1d0b697cc946bc050a75c25203f0266c0baa47fe
Harsh Jha <harshjha872@gmail.com>
2025-01-09 09:00:53 +0530

    mobile design

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-"use client"
+"use client";
 
 // import { useAppSelector, useAppDispatch } from "./hooks";
 // import { increaseCounter, decreaseCounter } from "@/store/todoSlice";
+import { ChevronDown, Search } from "lucide-react";
 import Navbar from "@/components/navbar/navbar";
+import Accordian from "@/components/accordian/accordian";
 export default function Home() {
   // const dispatch = useAppDispatch()
   // const counter = useAppSelector((state) => state.todoSlice.counter);
@@ -10,7 +12,43 @@ export default function Home() {
   return (
     <div className="">
       <Navbar />
-      Hi
+      <div className="py-4 px-4 flex justify-end">
+        <button className="bg-fuchsia-800 text-white px-6 py-3 rounded-full uppercase text-sm font-medium">
+          Add task
+        </button>
+      </div>
+      <div className="px-4">
+        <div className="text-neutral-500 mb-2">Filter by:</div>
+        <div className="flex space-x-2">
+          <div className="flex space-x-2 items-center px-4 py-2 border border-neutral-400 shadow-sm rounded-full text-neutral-600">
+            <span>Category</span>
+            <ChevronDown size={15} />
+          </div>
+          <div className="flex space-x-2 items-center px-6 py-2 border border-neutral-400 shadow-sm rounded-full text-neutral-600">
+            <span>Due date</span>
+            <ChevronDown size={15} />
+          </div>
+        </div>
+      </div>
+
+      <div className="px-4 w-full py-6 relative">
+        <input
+          placeholder="search"
+          className="border-[1.5px] border-neutral-300 rounded-full py-3 px-11 w-full"
+          type="text"
+        />
+        <div
+          className={`absolute top-1/2 transform -translate-y-1/2 text-gray-400 left-8`}
+        >
+          <Search size={20} />
+        </div>
+      </div>
+
+      <div className="px-4 space-y-4 flex flex-col items-center justify-center lg:justify-start lg:items-start">
+        <Accordian title="Todo" Tasks={[]} header_color="bg-pink-200" />
+        <Accordian title="In-progress" Tasks={[]} header_color="bg-sky-200" />
+        <Accordian title="Completed" Tasks={[]} header_color="bg-green-200" />
+      </div>
     </div>
   );
 }
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -5,20 +5,22 @@ import { NotepadText } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <div className="flex w-full justify-between items-center px-20 mt-12">
-      <div className="flex space-x-2">
-        <NotepadText size={25} />
-        <span>TaskBuddy</span>
+    <div className="flex w-full justify-between items-center px-6 py-4 shadow-md shadow-neutral-200 bg-pink-100  border-b ">
+      <div className="flex md:space-x-2 items-center">
+        <NotepadText className="hidden md:block" size={25} />
+        <span className="text-lg">TaskBuddy</span>
       </div>
-      <div className="flex space-x-2">
+      <div className="flex space-x-2 items-center">
+        <div className="relative h-8 w-8">
         <Image
           alt="userimage"
+          className="rounded-full"
+          layout="fill"
           src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80"
-          width={100}
-          height={100}
+          objectFit="cover"
         />
-
-        <div>user name</div>
+        </div>
+        <div className="hidden md:block">Harsh Jha</div>
       </div>
     </div>
   );
